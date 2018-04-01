ce02d7659bfecca2179bb0e9e6b42f94b4af470f
Abel Silva <abel.silva@gmail.com>
2018-04-01 16:52:35 +0100

    fix start script error

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@
 mkdir -p /srv/unifi-controller/logs
 mkdir -p /srv/unifi-controller/data
 mkdir -p /srv/unifi-controller/run
-rm -f /usr/lib/unifi-controller/logs
-rm -f /usr/lib/unifi-controller/data
-rm -f /usr/lib/unifi-controller/run
+rm -f /usr/lib/unifi/logs
+rm -f /usr/lib/unifi/data
+rm -f /usr/lib/unifi/run
 ln -s /srv/unifi-controller/logs /usr/lib/unifi/logs
 ln -s /srv/unifi-controller/data /usr/lib/unifi/data
 ln -s /srv/unifi-controller/run  /usr/lib/unifi/run
